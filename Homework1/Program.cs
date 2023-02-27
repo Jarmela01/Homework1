8f6e6cd04f7d310f5d1c34ced45c90100efc1126
Your Name <you@example.com>
2023-02-27 23:09:37 +0400

    ADD Homework

---
+++ Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sergi Jarmelishvili");
+            Console.WriteLine("Please Enter Words");
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
